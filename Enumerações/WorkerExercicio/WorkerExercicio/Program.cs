065ed4a83647b9a14a9c0d1eb781604065bb8776
Gustavo <gustavo.souza.dev10@gmail.com>
2021-03-06 13:00:59 -0300

    Exercicio Worker

---
+++ Enumerações/WorkerExercicio/WorkerExercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkerExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
