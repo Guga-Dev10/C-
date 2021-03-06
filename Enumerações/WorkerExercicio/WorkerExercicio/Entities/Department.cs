3c508623abee64bbade4b739cb0145580899a9ae
Gustavo <gustavo.souza.dev10@gmail.com>
2021-03-06 18:20:29 -0300

    Programa Worker desenvolvido para calcular Contratos, Nome da profissao, Salario, Ganhos mensais e etc

---
+++ Enumerações/WorkerExercicio/WorkerExercicio/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerExercicio.Entities
+{
+    class Department
+    {
+        public string DepartmentName { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            DepartmentName = name;
+        }
+    }
+}
--- Enumerações/WorkerExercicio/WorkerExercicio/Program.cs
+++ Enumerações/WorkerExercicio/WorkerExercicio/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using WorkerExercicio.Entities;
+using WorkerExercicio.Entities.Enums;
+
 
 namespace WorkerExercicio
 {
@@ -6,7 +11,53 @@ namespace WorkerExercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter department's name = ");
+            string deptName = Console.ReadLine();
+            
+            Console.WriteLine();
+            Console.Write("Enter Worker Data:");
+            Console.WriteLine();
+
+            Console.Write("Name = ");
+            string workerName = Console.ReadLine();
+            Console.Write("Level (Junior/Midlevel/Senior) = ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine()); // Conversão String-Enum
+            Console.Write("Base Salary = ");
+            double baseSalary = double.Parse(Console.ReadLine(), CI);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(workerName, level, baseSalary, dept);
+
+            Console.WriteLine();
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (MM/DD/YYYY) = ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour = ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Duration (hours) = ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY) = ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine();
+            Console.WriteLine("Worker Name = " + worker.WorkerName);
+            Console.WriteLine("Department = " + worker.Department.DepartmentName);
+            Console.WriteLine("Income for " + monthAndYear + ": $" + worker.Income(year, month));
         }
     }
 }
