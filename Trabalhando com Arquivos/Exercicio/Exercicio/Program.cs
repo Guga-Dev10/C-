f1d240a714955634464713a47bcdda68225409ba
Gustavo <gustavo.souza.dev10@gmail.com>
2021-06-05 11:47:29 -0300

    Meu notebook

---
+++ Trabalhando com Arquivos/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Gustavo Souza\Desktop\Curso C#\Trabalhando com Arquivos\Exercicio\Produtos.csv";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                // Consegui pegar o arquivo e ler com o .NET
+                // Ele está separado por nome, preço e quantidade
+            }
+        }
+    }
+}
