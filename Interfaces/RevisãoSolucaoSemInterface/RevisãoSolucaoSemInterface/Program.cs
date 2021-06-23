5fca2123130f102632c7a5750bc66a19ca3a3605
Gustavo <gustavo.souza.dev10@gmail.com>
2021-06-23 11:03:09 -0300

    Revisão de Solução sem o uso de Interface

---
+++ Interfaces/RevisãoSolucaoSemInterface/RevisãoSolucaoSemInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RevisãoSolucaoSemInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
