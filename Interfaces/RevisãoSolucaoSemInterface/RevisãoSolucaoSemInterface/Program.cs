7fe4cb4978a52d9a3fd6c35c0ba16d5fe7f84c19
Gustavo <gustavo.souza.dev10@gmail.com>
2021-06-29 11:55:54 -0300

    Iniciando uma revisão de conteúdo em C#

--- Interfaces/RevisãoSolucaoSemInterface/RevisãoSolucaoSemInterface/Program.cs
+++ Interfaces/RevisãoSolucaoSemInterface/RevisãoSolucaoSemInterface/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace RevisãoSolucaoSemInterface
 {
@@ -6,7 +7,14 @@ namespace RevisãoSolucaoSemInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data: ");
+            Console.Write("Car Model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup MM/dd/yyyy HH:mm = ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return MM/dd/yyyy HH:mm = ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
+
         }
     }
 }
---
+++ Rev2/Triângulo/Triângulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
