18f508f4cd21bb15a1ebc1028c33d79521636562
Gustavo <gustavo.souza.dev10@gmail.com>
2021-06-29 13:27:51 -0300

    Revisão - Classe - Triângulo

---
+++ Rev2/Triângulo/Triângulo/Entities/Triângulos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Triângulo.Entities
+{
+    class Triângulos
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triângulos()
+        {
+        }
+        public Triângulos(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double perimetroTriangulo()
+        {
+            double perimetro = 0;
+            return perimetro = (A + B + C) / 2;
+        }
+        public double areaTriangulo()
+        {
+            double p = perimetroTriangulo();
+            double area = 0;
+            return area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
--- Rev2/Triângulo/Triângulo/Program.cs
+++ Rev2/Triângulo/Triângulo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Triângulo.Entities;
 
 namespace Triângulo
 {
@@ -6,7 +7,26 @@ namespace Triângulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Insira o valor dos triângulos para o cálculo do perimetro! \n");
+            Console.Write("Insira os valores do Triângulo A: \n");
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+
+            Triângulos TA = new Triângulos(A, B, C);
+
+            Console.WriteLine();
+            Console.Write("Insira os valores do Triângulo B: \n");
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+            
+            Triângulos TB = new Triângulos(A, B, C);
+
+            Console.WriteLine("Perimetro Triângulo A = " + TA.perimetroTriangulo());
+            Console.WriteLine("Area A = " + TA.areaTriangulo());
+            Console.WriteLine("Perimetro Triângulo B = " + TB.perimetroTriangulo());
+            Console.WriteLine("Area B = "  + TB.areaTriangulo());
         }
     }
 }
