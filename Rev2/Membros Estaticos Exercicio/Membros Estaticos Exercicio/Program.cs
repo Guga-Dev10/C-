89292da99b738f464b8367fcf5db7f82b00b3b50
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-07-17 14:57:06 -0300

    Iniciando exercícios com membros estáticos

---
+++ Rev2/Membros Estaticos Exercicio/Membros Estaticos Exercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Membros_Estaticos_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
