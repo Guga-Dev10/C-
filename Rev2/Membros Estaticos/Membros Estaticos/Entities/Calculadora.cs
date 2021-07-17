9951c318ff01d22171ff2d2e5495cf87a90b54be
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-07-17 14:54:21 -0300

    Revisão de membros estáticos

---
+++ Rev2/Membros Estaticos/Membros Estaticos/Entities/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+namespace Membros_Estaticos.Entities
+{
+    class Calculadora
+    {
+        /* colocando um comando static nas funções permite que eles possam ser chamados 
+         * em outras partes do código sem precisar de instânciação 
+         */
+
+        public static double Pi = 3.14;
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4 / 3 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
