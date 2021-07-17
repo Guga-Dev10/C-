30d582c301e7576a509107e8b8bd340dc407602e
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-07-17 18:24:14 -0300

    Terminado o exercício de membros estáticos

---
+++ Rev2/Membros Estaticos Exercicio/Membros Estaticos Exercicio/Entities/ConversorDeMoedas.cs
@@ -0,0 +1,13 @@
+﻿namespace Membros_Estaticos_Exercicio.Entities
+{
+    class ConversorDeMoedas
+    {
+        public static double Conversor(double cotacaoDolar, double qteDolar)
+        {
+            double IOF = 0.06;
+            double qte = cotacaoDolar + cotacaoDolar * IOF;
+            qte *= qteDolar;
+            return qte;
+        }
+    }
+}
--- Rev2/Membros Estaticos Exercicio/Membros Estaticos Exercicio/Program.cs
+++ Rev2/Membros Estaticos Exercicio/Membros Estaticos Exercicio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Membros_Estaticos_Exercicio.Entities;
 
 namespace Membros_Estaticos_Exercicio
 {
@@ -6,7 +7,15 @@ namespace Membros_Estaticos_Exercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine());
+            Console.Write("Qual a quantidade de doláres a serem comprados? ");
+            double qteDolar = double.Parse(Console.ReadLine());
+
+            double valorEmReais = ConversorDeMoedas.Conversor(cotacaoDolar, qteDolar);
+            Console.WriteLine($"\n Cotação do dolar = {cotacaoDolar} " +
+                $"\nDolares Comprados: {qteDolar}" +
+                $"\nValor total em Reais: {valorEmReais}");
         }
     }
 }
