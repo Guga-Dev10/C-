694aa0fc289d52bb82290b2552a01da08c6df2ff
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-11-06 13:47:20 -0300

    revisão de introdução ao POO

--- Rev3/TrianguloComPoo/TrianguloComPoo/Program.cs
+++ Rev3/TrianguloComPoo/TrianguloComPoo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TrianguloComPoo
 {
@@ -6,7 +7,29 @@ namespace TrianguloComPoo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            Triangulo X, Y;
+
+            X = new Triangulo();
+            Y = new Triangulo();
+
+            Console.WriteLine("Entre com os dados do triângulo X: ");
+            X.A = double.Parse(Console.ReadLine(), CI);
+            X.B = double.Parse(Console.ReadLine(), CI);
+            X.C = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("\nEntre com os dados do Triângulo Y: ");
+            Y.A = double.Parse(Console.ReadLine(), CI);
+            Y.B = double.Parse(Console.ReadLine(), CI);
+            Y.C = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("\nResultados do triângulo X: " +
+                "\nPerimetro = " + X.PerimetroTriangulo().ToString("F2",CI)
+                + "\nÁrea = " + X.AreaTriangulo().ToString("F2", CI));
+            
+            Console.WriteLine("\nResultados do triângulo Y: " +
+                "\nPerimetro = " + Y.PerimetroTriangulo().ToString("F2",CI)
+                + "\nÁrea = " + Y.AreaTriangulo().ToString("F2", CI));
         }
     }
 }
