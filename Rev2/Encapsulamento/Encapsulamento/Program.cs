00103d127a996a67cda7e3be91a9438c073fc357
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-11-06 11:06:25 -0300

    Retomando o aprendizado de Interfaces

--- Rev2/Encapsulamento/Encapsulamento/Program.cs
+++ Rev2/Encapsulamento/Encapsulamento/Program.cs
@@ -6,7 +6,15 @@ namespace Encapsulamento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /**
+             *  Encapsulamento: é um princípio que consiste em esconder detalhes de implementação
+             *  de um componente, expondo apenas operações seguras e que o mantenha em um estado consistente.
+             *  
+             *  Regra de ouro> o objeto deve sempre estar em um estado consistente, e a própria classe deve garantir
+             *  isso.
+             *  
+             */
+            Console.WriteLine("ola");
         }
     }
 }
