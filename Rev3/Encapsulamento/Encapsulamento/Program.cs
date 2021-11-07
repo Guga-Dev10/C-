931fdfcafa1f72f30022198d94f145d9031b674e
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-11-07 10:53:09 -0300

    Usando VS Code para executar um programa C#

--- Rev3/Encapsulamento/Encapsulamento/Program.cs
+++ Rev3/Encapsulamento/Encapsulamento/Program.cs
@@ -6,7 +6,7 @@ namespace Encapsulamento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World usando VS Code!");
         }
     }
 }
