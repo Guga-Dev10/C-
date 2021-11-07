fb5d8e3b242d2e985fcf7006e2c9ac882481a77f
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-11-07 10:17:21 -0300

    Revisão de Membros estáticos. Exercício proposto de desenvolvimento de uma calculadora de cotação de dólar

--- Rev3/MembrosEstaticos/MembrosEstaticos/Program.cs
+++ Rev3/MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -6,7 +6,15 @@ namespace MembrosEstaticos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Qual a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade a ser comprada? ");
+            double quantidade = double.Parse(Console.ReadLine());
+            
+
+            double cotacaoDoDolar = CotacaoDolar.Calculadora(dolar, quantidade);
+
+            Console.Write("\nA cotação do dólar é: R$" + cotacaoDoDolar);
         }
     }
 }
