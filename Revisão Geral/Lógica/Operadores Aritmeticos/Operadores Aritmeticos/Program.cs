49cb5b41f17c01b4e762425c6428e096cef3359b
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-05-15 13:18:11 -0300

    operadores aritméticos

---
+++ Revisão Geral/Lógica/Operadores Aritmeticos/Operadores Aritmeticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operadores_Aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
