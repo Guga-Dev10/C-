cff6234bea6ef18cc0ea5f3c16c434281ad8200a
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-05-16 21:49:47 -0300

    Exercício 4

--- Revisão Geral/Lógica/Exercicios1/Exercicios1/Program.cs
+++ Revisão Geral/Lógica/Exercicios1/Exercicios1/Program.cs
@@ -38,12 +38,41 @@ namespace Exercicios1
              */
 
             Console.WriteLine("\n");
+            Console.WriteLine("Entre com o valor de quatro números inteiros: ");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
 
+            int diferencaProduto = (A * B) - (C * D);
+            Console.WriteLine($"Diferença entre os Produtos = {diferencaProduto}");
+
+            // Exercício 4
+
+            /* Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que 
+             recebe por hora e calcule o salário do funcionário.
+             */
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Entre com os dados do funcionário: \n");
+            Console.Write("Número do funcionário: ");
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+            Console.Write("Horas trabalhadas: ");
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            Console.Write("Valor da hora trabalhada: ");
+            double valorHora = double.Parse(Console.ReadLine());
+
+            double SalarioFuncionario = Salario(horasTrabalhadas, valorHora);
+            Console.WriteLine($"Dados do funcionário: \nNúmero = {numeroFuncionario}, Salário = ${SalarioFuncionario.ToString("F2")}");
         }
         static double circunferencia(double raio)
         {
             double area = Math.PI * Math.Pow(raio, 2);
             return area;
+        }
+        static double Salario(int horasTrabalhadas, double valorHora)
+        {
+            return horasTrabalhadas * valorHora;
         }
     }
 }
