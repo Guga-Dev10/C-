4715fd21e4d801856504f09ebf33cf2ca03c7b14
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-07-25 22:52:53 -0300

    Adicionando anotações na classe filme

--- APIs/Filmes/FilmesAPI/FilmesAPI/Models/Filme.cs
+++ APIs/Filmes/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        // Adicionando restrições à classe
+
+        [Required(ErrorMessage = "O Campo 'Diretor' é obrigatório!")]
         public string Diretor { get; set; }
+        [Required(ErrorMessage = "O Campo 'Título' é obrigatório!")]
         public string Titulo { get; set; }
+        [StringLength(30, ErrorMessage = "O Campo 'GÇenero' não pode ultrapassar 30 caracteres!")]
         public string Genero { get; set; }
+        [Range(1, 400, ErrorMessage = "A duração do filme deve ter entre 1 e 400 minutos!")]
         public int Duracao { get; set; }
     }
 }
