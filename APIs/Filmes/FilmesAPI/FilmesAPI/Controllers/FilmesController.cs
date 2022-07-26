728188d318d0ce30958ce3fc7a9fb4276a76473c
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-07-25 22:13:17 -0300

    Primeiro funcionamento da API

--- APIs/Filmes/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
+++ APIs/Filmes/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace FilmesAPI.Controllers
     [Route("controller")] //Quando a rota tiver o nome do controlador a API será chamada
     public class FilmesController : ControllerBase
     {
+        List<Filme> filmes = new List<Filme>();
 
+        public void adicionarFilmes([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
     }
 }
