0ad37e016e941e68a8aa810658b900936def64de
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-07-25 22:02:16 -0300

    Model Objetos Filme

--- APIs/Filmes/FilmesAPI/FilmesAPI/Models/Filme.cs
+++ APIs/Filmes/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -7,6 +7,9 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
-
+        public string Diretor { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
     }
 }
