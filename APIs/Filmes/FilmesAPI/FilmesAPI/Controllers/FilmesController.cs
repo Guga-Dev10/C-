b65d328068942e74a98d9fb9f4ffeb5e273da447
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2022-07-25 21:59:42 -0300

    Model Filme Criada

---
+++ APIs/Filmes/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController] //Define a classe como sendo uma classe controladora
+    [Route("controller")] //Quando a rota tiver o nome do controlador a API será chamada
+    public class FilmesController : ControllerBase
+    {
+
+    }
+}
